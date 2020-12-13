7ac4257bdb5cbab1e6512935d0c36b7282b29685
Stanislaw Kuich <stanislaw.kuich@gmail.com>
2020-12-13 09:20:59 +0100

    remove login resources

--- integration/vagrant-chef/dockerHost/attributes/default.rb
+++
@@ -1,1 +0,0 @@
-default['github_token'] = 'd91d1cbafc50bde45352a3acd23a8d871fe32738'
--- integration/vagrant-chef/dockerHost/recipes/default.rb
+++ integration/vagrant-chef/dockerHost/recipes/default.rb
@@ -11,11 +11,6 @@ service 'docker' do
     action [:enable, :start]
 end
   
-execute 'login to docker registry' do
-    command "docker login ghcr.io -u pu5zkin -p #{node['github_token']}"
-    sensitive true
-end
-
 execute 'run docker image' do
-    command 'docker pull ghcr.io/pu5zkin/spa/spa:latest'
+    command 'docker run --rm -d -p 4200:4200/tcp ghcr.io/pu5zkin/spa/spa:latest'
 end
